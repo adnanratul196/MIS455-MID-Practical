9064c14921d533dab1435cb212461d0299ea0897
adnanratul196 <adnanratul196@gmail.com>
2026-07-06 02:11:37 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function shopNow() {
+    window.location.href = "products.html";
+}
+function buyProduct(product){
+
+alert(product + " has been added to your cart!");
+
+}
+function sendMessage(){
+
+alert("Thank you! Your message has been sent successfully.");
+
+}
+function subscribe(){
+
+alert("Thank you for subscribing to TechZone!");
+
+}
